fb80bfc42929c8aba0d1394f33036ed320b5b807
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2019-10-25 16:17:28 +0200

    update harnesses more

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
 // js code here
+
+function startAtom() {
+  let sheetLink = getSheet();
+  hideAtom();
+}
+
+function getSheet() {
+  console.log('this will return the sheet');
+}
+
+function hideAtom() {
+  console.log('this will hide the atom');
+}
+
